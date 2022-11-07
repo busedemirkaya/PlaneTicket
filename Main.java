fdf9636b3ed3373cbdb1473b543d8591c3eb63a4
Buse Demirkaya <112807956+busedemirkaya@users.noreply.github.com>
2022-11-07 21:52:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /* Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan program.
+          
+          Mesafe başına ücret 0,10 TL.
+          İndirimler:
+          Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
+          Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
+          Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
+          Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır. */
         int yas, mesafe, yolculukTipi;
         double mesafeUcreti = 0.10;
         double OnIkıdenKucukse = 0.50;
